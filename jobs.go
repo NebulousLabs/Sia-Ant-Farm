2ed45baab1f7cc51c44fd4e8db8d79648a5807b6
Johnathan Howell <me@johnathanhowell.com>
2016-08-15 19:44:42 -0400

    use boolean flags to enable jobs

--- jobs.go
+++ jobs.go
@@ -7,19 +7,29 @@ import (
 	"github.com/NebulousLabs/Sia/api"
 )
 
-// JobPeerConnectability will print an error to the log if the node has zero
+type JobRunner struct {
+	errorlog chan interface{}
+}
+
+func NewJobRunner() *JobRunner {
+	return &JobRunner{
+		errorlog: make(chan interface{}),
+	}
+}
+
+// gatewayConnectability will print an error to the log if the node has zero
 // peers at any time.
-func JobPeerConnectability(log chan interface{}) {
+func (j *JobRunner) gatewayConnectability() {
 	for {
 		time.Sleep(time.Second * 5)
 		var info api.GatewayGET
 		err := getAPI("/gateway", &info)
 		if err != nil {
-			log <- fmt.Sprintf("Error in JobPeerConnectability: %v\n", err)
+			j.errorlog <- fmt.Sprintf("Error in JobPeerConnectability: %v\n", err)
 			return
 		}
 		if len(info.Peers) == 0 {
-			log <- "JobPeerConnectability: node has zero peers..."
+			j.errorlog <- "JobPeerConnectability: node has zero peers..."
 		}
 	}
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"time"
 )
 
 // NewSiad spawns a new siad process using os/exec.  siadPath is the path to
@@ -21,32 +22,41 @@ func NewSiad(siadPath string) (*exec.Cmd, error) {
 
 func main() {
 	siadPath := flag.String("siad", "siad", "path to siad executable")
+	runGateway := flag.Bool("gateway", false, "enable gateway test jobs")
 	flag.Parse()
+
+	// Construct a new siad instance
 	siad, err := NewSiad(*siadPath)
 	if err != nil {
 		panic(err)
 	}
 
-	// Construct the logger channel
-	logchan := make(chan interface{})
+	// Construct the job runner
+	j := NewJobRunner()
 
-	// Construct the signal channel
+	// Construct the signal channel and notify on it in the case of SIGINT
+	// (ctrl-c)
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt)
+
+	// Concurrently print errors or kill siad and quit on ctrl-c
 	go func() {
 		for {
 			select {
 			case <-sigchan:
 				siad.Process.Kill()
 				return
-			case msg := <-logchan:
-				fmt.Println(msg)
+			case err := <-j.errorlog:
+				fmt.Printf("%v: %v\n", time.Now(), err)
 			}
 		}
 	}()
 
-	// Start up a Peer Connectability job
-	go JobPeerConnectability(logchan)
+	// Start up siad jobs
+	if *runGateway {
+		fmt.Println("running gateway connectability job...")
+		go j.gatewayConnectability()
+	}
 
 	// Wait for the siad process to return an error.  Ignore the error if it's a
 	// SIGKILL, since we issue the process SIGKILL on quit.
