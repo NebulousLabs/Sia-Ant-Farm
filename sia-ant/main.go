de53892099da690e0711aad6658cb4ac811cfae7
Johnathan Howell <me@johnathanhowell.com>
2016-08-18 19:56:01 -0400

    autogenerate free addresses on localhost

--- sia-ant/main.go
+++ sia-ant/main.go
@@ -4,24 +4,60 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
 	"os/signal"
 	"time"
 )
 
+type Siad struct {
+	hostAddr string
+	rpcAddr  string
+	apiAddr  string
+
+	cmd *exec.Cmd
+}
+
+// getAddrs returns n free listening ports on localhost by leveraging the
+// behaviour of net.Listen("localhost:0").
+func getAddrs(n int) ([]string, error) {
+	var addrs []string
+
+	for i := 0; i < n; i++ {
+		l, err := net.Listen("tcp", "localhost:0")
+		if err != nil {
+			return nil, err
+		}
+		defer l.Close()
+		addrs = append(addrs, l.Addr().String())
+	}
+	return addrs, nil
+}
+
 // NewSiad spawns a new siad process using os/exec.  siadPath is the path to
 // Siad, passed directly to exec.Command.  An error is returned if starting
 // siad fails, otherwise a pointer to siad's os.Cmd object is returned.  The
-// data directory `datadir` is passed as siad's `--sia-directory`
-func NewSiad(siadPath string, datadir string) (*exec.Cmd, error) {
-	cmd := exec.Command(siadPath, "--sia-directory", datadir)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); err != nil {
+// data directory `datadir` is passed as siad's `--sia-directory`.  3 open
+// ports will be assigned as Sia's api, rpc, and host ports.
+func NewSiad(siadPath string, datadir string) (*Siad, error) {
+	// get 3 available bind addresses
+	addrs, err := getAddrs(3)
+	if err != nil {
 		return nil, err
 	}
-	return cmd, nil
+	siad := &Siad{
+		apiAddr:  addrs[0],
+		rpcAddr:  addrs[1],
+		hostAddr: addrs[2],
+	}
+	siad.cmd = exec.Command(siadPath, "--sia-directory", datadir, "--api-addr", siad.apiAddr, "--rpc-addr", siad.rpcAddr, "--host-addr", siad.hostAddr)
+	siad.cmd.Stdout = os.Stdout
+	siad.cmd.Stderr = os.Stderr
+	if err := siad.cmd.Start(); err != nil {
+		return nil, err
+	}
+	return siad, nil
 }
 
 func main() {
@@ -46,13 +82,14 @@ func main() {
 	siad, err := NewSiad(*siadPath, datadir)
 	if err != nil {
 		panic(err)
+		return
 	}
 
 	// Naively wait for the daemon to start.
 	time.Sleep(time.Second)
 
 	// Construct the job runner
-	j, err := NewJobRunner("localhost:9980", "")
+	j, err := NewJobRunner(siad.apiAddr, "")
 	if err != nil {
 		panic(err)
 	}
@@ -68,7 +105,7 @@ func main() {
 			select {
 			case <-sigchan:
 				fmt.Println("Caught quit signal, quitting...")
-				siad.Process.Kill()
+				siad.cmd.Process.Kill()
 				return
 			case err := <-j.errorlog:
 				fmt.Printf("%v: %v\n", time.Now(), err)
@@ -91,7 +128,7 @@ func main() {
 	// Wait for the siad process to return an error.  Ignore the error if it's a
 	// SIGKILL, since we issue the process SIGKILL on quit.
 	fmt.Println("> all jobs loaded.")
-	err = siad.Wait()
+	err = siad.cmd.Wait()
 	if err != nil && err.Error() != "signal: killed" {
 		panic(err)
 	}
