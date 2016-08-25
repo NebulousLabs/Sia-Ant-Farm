e0b374121370f9a5c956c81c9de533153f681a2b
Johnathan Howell <me@johnathanhowell.com>
2016-08-25 14:58:38 -0400

    print a warning in gateway job if node has zero peers

--- sia-ant/job_gateway.go
+++ sia-ant/job_gateway.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"time"
+
+	"github.com/NebulousLabs/Sia/api"
 )
 
 // gatewayConnectability will print an error to the log if the node has zero
@@ -10,10 +12,14 @@ import (
 func (j *JobRunner) gatewayConnectability() {
 	for {
 		time.Sleep(time.Second * 5)
-		err := j.client.Get("/gateway", nil)
+		var gatewayInfo api.GatewayGET
+		err := j.client.Get("/gateway", &gatewayInfo)
 		if err != nil {
 			log.Printf("[%v gatewayConnectability ERROR]: %v\n", j.siaDirectory, err)
 			return
+		}
+		if len(gatewayInfo.Peers) == 0 {
+			log.Printf("[%v gatewayConnectability WARN]: ant has zero peers", j.siaDirectory)
 		}
 	}
 }
