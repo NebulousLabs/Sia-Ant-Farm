e111f5455fdb3cb66541414f5757baff87ceff98
Johnathan Howell <me@johnathanhowell.com>
2016-09-08 16:52:32 -0400

    decrease frequency of logging, fix struct log flag

--- sia-ant/job_host.go
+++ sia-ant/job_host.go
@@ -89,7 +89,7 @@ func (j *JobRunner) jobHost() {
 		select {
 		case <-j.tg.StopChan():
 			return
-		case <-time.After(time.Second):
+		case <-time.After(time.Second * 15):
 		}
 
 		var hostInfo api.HostGET
@@ -97,8 +97,8 @@ func (j *JobRunner) jobHost() {
 		if err != nil {
 			log.Printf("[%v jobHost ERROR]: %v\n", j.siaDirectory, err)
 		}
-		log.Printf("[%v jobHost INFO]: (+%v)\n", j.siaDirectory, hostInfo.NetworkMetrics)
-		log.Printf("[%v jobHost INFO]: (+%v)\n", j.siaDirectory, hostInfo.FinancialMetrics)
+		log.Printf("[%v jobHost INFO]: %+v\n", j.siaDirectory, hostInfo.NetworkMetrics)
+		log.Printf("[%v jobHost INFO]: %+v\n", j.siaDirectory, hostInfo.FinancialMetrics)
 
 		// Print an error if storage revenue has decreased
 		if hostInfo.FinancialMetrics.StorageRevenue.Cmp(maxRevenue) >= 0 {
