1c967c34bd50028d40b6d4f8dd1a0fe6daecbe57
David Vorick <david@nebulouslabs.com>
2016-09-14 06:46:48 -0400

    refine gateway job error detection

--- sia-ant/job_gateway.go
+++ sia-ant/job_gateway.go
@@ -12,25 +12,35 @@ import (
 func (j *JobRunner) gatewayConnectability() {
 	done := make(chan struct{})
 	defer close(done)
-
 	j.tg.OnStop(func() {
 		<-done
 	})
 
+	// Initially wait a while to give the other ants some time to spin up.
+	select {
+	case <-j.tg.StopChan():
+		return
+	case <-time.After(time.Minute):
+	}
+
 	for {
+		// Wait 30 seconds between iterations.
 		select {
 		case <-j.tg.StopChan():
 			return
-		case <-time.After(time.Second * 5):
+		case <-time.After(time.Second * 30):
 		}
 
+		// Count the number of peers that the gateway has. An error is reported
+		// for less than two peers because the gateway is likely connected to
+		// itself.
 		var gatewayInfo api.GatewayGET
 		err := j.client.Get("/gateway", &gatewayInfo)
 		if err != nil {
-			log.Printf("[%v gatewayConnectability ERROR]: %v\n", j.siaDirectory, err)
+			log.Printf("[ERROR] [gateway] [%v] error when calling /gateway: %v\n", j.siaDirectory, err)
 		}
-		if len(gatewayInfo.Peers) == 0 {
-			log.Printf("[%v gatewayConnectability WARN]: ant has zero peers", j.siaDirectory)
+		if len(gatewayInfo.Peers) < 2 {
+			log.Printf("[ERROR] [gateway] [%v] ant has less than two peers: %v\n", j.siaDirectory, gatewayInfo.Peers)
 		}
 	}
 }
